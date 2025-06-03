3f6437d78ab5140c9c3a5135d1499c5a256aa180
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2025-06-03 09:20:47 -0400

    Envio de projeto para repositorio remoto

---
+++ src/main/java/com/wbaamaral/brasileirao_api/BrasileiraoApiApplication.java
@@ -0,0 +1,13 @@
+package com.wbaamaral.brasileirao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbaamaral/brasileirao_api/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbaamaral.brasileirao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
